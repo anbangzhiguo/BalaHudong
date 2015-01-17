840423126a21eb78d1a7951fb9d6ae51baf8d6b6
anbangzhiguo <anbangzhiguo@163.com>
2015-01-17 17:14:57 +0800

    update

--- BalaHudong/src/com/balahudong/interceptors/LoginCheckInerceptors.java
+++ BalaHudong/src/com/balahudong/interceptors/LoginCheckInerceptors.java
@@ -6,6 +6,7 @@ import com.jfinal.core.ActionInvocation;
 public class LoginCheckInerceptors implements Interceptor{
 	@Override
 	public void intercept(ActionInvocation ai) {
+		
 		System.out.println("Before action invoking");
 		ai.invoke();
 		System.out.println("After action invoking");
