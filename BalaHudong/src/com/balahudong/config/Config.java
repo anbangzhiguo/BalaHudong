913aecc158aca264ac32e310993df98ba5ab975a
anbangzhiguo <anbangzhiguo@163.com>
2015-01-17 14:14:59 +0800

    balahudong

---
+++ BalaHudong/src/com/balahudong/config/Config.java
@@ -0,0 +1,51 @@
+package com.balahudong.config;
+
+import java.util.Properties;
+
+import com.balahudong.controller.SendMessageController;
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+
+public class Config extends JFinalConfig {
+
+	public Properties loadProp(String pro, String dev) {
+		try {
+			return loadPropertyFile(pro);
+		} catch (Exception e) {
+			return loadPropertyFile(dev);
+		}
+	}
+
+	@Override
+	public void configConstant(Constants me) {
+		loadProp("a_little_config_pro.txt", "a_little_config.txt");
+		me.setDevMode(getPropertyToBoolean("devMode", false));
+		me.setDevMode(me.getDevMode());
+	}
+
+	@Override
+	public void configRoute(Routes me) {
+		me.add("/sendMessage", SendMessageController.class);
+
+	}
+
+	@Override
+	public void configPlugin(Plugins me) {
+
+	}
+
+	@Override
+	public void configInterceptor(Interceptors me) {
+
+	}
+
+	@Override
+	public void configHandler(Handlers me) {
+
+	}
+
+}
---
+++ BalaHudong/src/com/balahudong/controller/SendMessageController.java
@@ -0,0 +1,12 @@
+package com.balahudong.controller;
+
+import com.jfinal.core.Controller;
+
+public class SendMessageController extends Controller {
+	public void index() {
+		renderText("Hello JFinalfdfdf World.");
+	}
+	public void index22() {
+		renderText("Hello JFinalfdfdf22222 World.");
+	}
+}
